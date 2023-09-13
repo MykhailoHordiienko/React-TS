337f4358f1fef6b6e52499909beb91b0bedf5579
Mykhailo Hordiienko <gordienkomichael191@gmail.com>
2023-09-13 18:00:13 +0300

    add Profile component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
 import './App.css';
+import user from '../public/user.json';
+import Profile from './components/Profile/Profile';
 
 function App() {
   return (
     <>
-      <h1>Lesson-1</h1>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
     </>
   );
 }
---
+++ src/components/Profile/Profile.tsx
@@ -0,0 +1,65 @@
+import css from './profile.module.css';
+
+type ProfileProps = {
+  username: string;
+  tag: string;
+  location: string;
+  avatar: string;
+  stats: {
+    followers: number;
+    views: number;
+    likes: number;
+  };
+};
+
+const Profile = ({
+  username,
+  tag,
+  location,
+  avatar,
+  stats: { followers, likes, views },
+}: ProfileProps) => {
+  return (
+    <div className={css.profile}>
+      <div className={css.description}>
+        <img
+          src={avatar}
+          alt="User avatar"
+          className={css.avatar}
+        />
+        <p className={css.name}>{username}</p>
+        <p className={css.tag}>{tag}</p>
+        <p className={css.location}>{location}</p>
+      </div>
+
+      <ul className={css.stats}>
+        <li>
+          <span className={css.label}>Followers</span>
+          <span
+          //   className={}
+          >
+            {followers}
+          </span>
+        </li>
+        <li>
+          <span className={css.label}>Views</span>
+          <span
+          //   className={}
+          >
+            {views}
+          </span>
+        </li>
+        <li>
+          <span className={css.label}>Likes</span>
+          <span
+          //   className={}
+          >
+            {likes}
+          </span>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Profile;
