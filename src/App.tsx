cd49ff2277295403cd422495cf090aadef4ac59c
Mykhailo Hordiienko <gordienkomichael191@gmail.com>
2023-09-14 14:55:06 +0300

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,7 @@
 import './App.css';
-import user from './json/user.json';
-import data from './json/data.json';
-import friends from './json/friends.json';
-import transactions from './json/transactions.json';
-import Profile from './components/Profile/Profile';
-import Statistics from './components/Statistics/Statistics';
-import FriendsList from './components/FriendsList/FriendsList';
-import TransactionHistory from './components/TransactionHistory/TransactionHistory';
 
 function App() {
-  console.log(data);
-  return (
-    <>
-      <Profile
-        username={user.username}
-        tag={user.tag}
-        location={user.location}
-        avatar={user.avatar}
-        stats={user.stats}
-      />
-      <Statistics title="Upload stats" stats={data} />
-      <Statistics stats={data} />
-      <FriendsList friends={friends} />
-      <TransactionHistory transactions={transactions} />
-    </>
-  );
+  return <div>Lesson-2</div>;
 }
 
 export default App;
